90503ccaa7686c286b695840bbe3f3f0965d19e1
descire <daiqingyunc@gmail.com>
2021-12-16 00:01:32 +0800

    feat(): 实现 FisherYates shuffle 算法

--- summary/demo.js
+++ summary/demo.js
@@ -3,3 +3,22 @@ const Hashids = require('../cjs/hashids').default;
 const hash = new Hashids('daiqingyun', 10);
 
 console.log(hash.encode(300204))
+
+
+// Fisher–Yates shuffle
+
+function shuffle(input) {
+    for (let i = input.length - 1; i > 0; i--) {
+        const k = Math.floor(Math.random() * (i + 1));
+        const element = input[k];
+        input[k] = input[i];
+        input[i] = element;
+
+        console.log(JSON.stringify(input))
+    }
+
+    return input;
+}
+
+
+console.log(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]))
